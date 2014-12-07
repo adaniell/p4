28bb82c3987b76d3a992cd0f0bfa23e796bf8aed
Ashley Daniell <adaniell@hbs.edu>
2014-12-07 03:45:42 -0500

    updated db for production

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'manager',
+            'username'  => 'root',
+            'password'  => 'BR6fZ1Og3b',
+        ),
+
+    ),
+
+);
